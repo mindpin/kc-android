27aa9524dd5752cab299a1feaaab7270648e617a
fushang318 <fushang318@gmail.com>
2014-08-07 15:52:28 +0800

    add ui mock model

---
+++ app/src/com/mindpin/kc_android/models/ui_mock/UIMockKnowledgePoint.java
@@ -0,0 +1,37 @@
+package com.mindpin.kc_android.models.ui_mock;
+
+import com.mindpin.kc_android.models.interfaces.IKnowledgeNet;
+import com.mindpin.kc_android.models.interfaces.IKnowledgePoint;
+import com.mindpin.kc_android.models.interfaces.ITutorial;
+
+import java.util.List;
+
+/**
+ * Created by fushang318 on 2014/8/7.
+ */
+public class UIMockKnowledgePoint implements IKnowledgePoint{
+    private String name;
+    private String desc;
+
+    @Override
+    public String get_name() {
+        return this.name;
+    }
+
+    @Override
+    public String get_desc() {
+        return this.desc;
+    }
+
+    @Override
+    public IKnowledgeNet get_knowledge_net() {
+        // TODO 硬编码创建假数据
+        return null;
+    }
+
+    @Override
+    public List<ITutorial> get_tutorial_list() {
+        // TODO 硬编码创建假数据
+        return null;
+    }
+}
