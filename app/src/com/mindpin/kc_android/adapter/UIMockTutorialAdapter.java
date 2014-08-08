b6b0f41247fb81c2c2b3fba78f684c1cdc5ecbc9
destinyd <destinyd_war@163.com>
2014-08-08 15:19:28 +0000

    add test.jpg for tutorial list item

--- app/src/com/mindpin/kc_android/adapter/UIMockTutorialAdapter.java
+++ app/src/com/mindpin/kc_android/adapter/UIMockTutorialAdapter.java
@@ -1,27 +1,21 @@
 package com.mindpin.kc_android.adapter;
 
 import android.app.Activity;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.mindpin.kc_android.R;
 import com.mindpin.kc_android.adapter.base.KnowledgeBaseAdapter;
 import com.mindpin.kc_android.models.interfaces.ITutorial;
-
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URL;
 
 /**
  * Created by fushang318 on 14-1-13.
  */
 public class UIMockTutorialAdapter extends KnowledgeBaseAdapter<ITutorial> {
+    final Activity activity;
     public UIMockTutorialAdapter(Activity activity) {
         super(activity);
+        this.activity = activity;
     }
 
     @Override
@@ -46,14 +40,6 @@ public class UIMockTutorialAdapter extends KnowledgeBaseAdapter<ITutorial> {
         view_holder.tv_title.setText(item.get_title());
         view_holder.tv_description.setText(item.get_desc());
         view_holder.tv_is_learned.setText("否");
-        URL newurl = null;
-        try {
-            newurl = new URL(item.get_icon_url());
-            Bitmap mIcon_val = BitmapFactory.decodeStream(newurl.openConnection().getInputStream());
-            view_holder.iv_cover.setImageBitmap(mIcon_val);
-        } catch (java.io.IOException e) {
-            e.printStackTrace();
-        }
     }
 
     private class ViewHolder implements BaseViewHolder {
