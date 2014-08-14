5189c7c2944d1b8429625e74d1acc21ae0010fa5
destinyd <destinyd_war@163.com>
2014-08-14 17:20:00 +0000

    KnowledgeNetActivity extends to RoboActivity

--- app/src/com/mindpin/kc_android/KnowledgeNetActivity.java
+++ app/src/com/mindpin/kc_android/KnowledgeNetActivity.java
@@ -2,29 +2,38 @@ package com.mindpin.kc_android;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.TabHost;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
-
+import android.widget.ToggleButton;
 import com.mindpin.android.loadingview.LoadingView;
+import com.mindpin.kc_android.adapter.KnowledgeNetPointListAdapter;
+import com.mindpin.kc_android.adapter.KnowledgeNetTutorialListAdapter;
 import com.mindpin.kc_android.models.interfaces.IKnowledgeNet;
+import com.mindpin.kc_android.models.interfaces.IKnowledgePoint;
+import com.mindpin.kc_android.models.interfaces.ITutorial;
 import com.mindpin.kc_android.network.DataProvider;
 import com.mindpin.kc_android.utils.KCAsyncTask;
+import roboguice.activity.RoboActivity;
 
-import java.io.Serializable;
-
-import roboguice.activity.RoboTabActivity;
+import java.util.List;
 
 /**
  * Created by dd on 14-8-7.
  */
-public class KnowledgeNetActivity extends RoboTabActivity {
+public class KnowledgeNetActivity extends RoboActivity implements View.OnClickListener {
     private IKnowledgeNet net;
-    TabHost tabHost;
-    TextView tv_description, tv_title;
+    private List<ITutorial> tutorials;
+    private List<IKnowledgePoint> points;
+    private TextView tv_description, tv_title;
     private String knowledge_net_id;
     private LoadingView loading_view;
+    private KnowledgeNetTutorialListAdapter adapter_tutorials;
+    private ListView lv_tutorials;
+    private ListView lv_points;
+    private KnowledgeNetPointListAdapter adapter_points;
+    private ToggleButton btn_tutorials, btn_points;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,6 +41,12 @@ public class KnowledgeNetActivity extends RoboTabActivity {
         setContentView(R.layout.knowledge_net);
         tv_title = (TextView) findViewById(R.id.tv_title);
         tv_description = (TextView) findViewById(R.id.tv_description);
+        lv_tutorials = (ListView) findViewById(R.id.lv_list_tutorials);
+        lv_points = (ListView) findViewById(R.id.lv_list_points);
+        btn_tutorials = (ToggleButton) findViewById(R.id.btn_tutorials);
+        btn_points = (ToggleButton) findViewById(R.id.btn_points);
+        btn_tutorials.setOnClickListener(this);
+        btn_points.setOnClickListener(this);
         loading_view = (LoadingView) findViewById(R.id.loading_view);
 
         this.knowledge_net_id = getIntent().getStringExtra("knowledge_net_id");
@@ -39,33 +54,8 @@ public class KnowledgeNetActivity extends RoboTabActivity {
 
     }
 
-    private void setTabs() {
-        addTab("教程",
-                KnowledgeNetTutorialsActivity.class);
-        addTab("知识点",
-                KnowledgeNetPointsActivity.class);
-    }
-
-    private void addTab(String labelId,
-                        Class<?> c, String extra, Serializable net) {
-        Intent intent = new Intent(this, c);
-        intent.putExtra(extra, net);
-        TabHost.TabSpec spec = tabHost.newTabSpec("tab" + labelId);
-
-        View tabIndicator = LayoutInflater.from(this).inflate(R.layout.knowledge_net_tab_text, getTabWidget(), false);
-        TextView title = (TextView) tabIndicator.findViewById(R.id.title);
-        title.setText(labelId);
-        spec.setIndicator(tabIndicator);
-        spec.setContent(intent);
-        tabHost.addTab(spec);
-    }
-
-    private void addTab(String labelId, Class<?> c) {
-        addTab(labelId, c, "net", net);
-    }
-
     private void get_datas() {
-        new KCAsyncTask<Void>(this){
+        new KCAsyncTask<Void>(this) {
 
             @Override
             protected void onPreExecute() throws Exception {
@@ -75,8 +65,8 @@ public class KnowledgeNetActivity extends RoboTabActivity {
             @Override
             public Void call() throws Exception {
                 net = DataProvider.get_knowledge_net(knowledge_net_id);
-                net.get_tutorial_list();
-                net.get_knowledge_point_list();
+                tutorials = net.get_tutorial_list();
+                points = net.get_knowledge_point_list();
                 return null;
             }
 
@@ -89,9 +79,61 @@ public class KnowledgeNetActivity extends RoboTabActivity {
     }
 
     private void build_view() {
-        tabHost = getTabHost();
-        setTabs();
         tv_title.setText(net.get_name());
         tv_description.setText(net.get_desc());
+
+        bind_tutorials();
+        bind_points();
+    }
+
+    private void bind_tutorials() {
+        adapter_tutorials = new KnowledgeNetTutorialListAdapter(this);
+        adapter_tutorials.add_items(tutorials);
+        lv_tutorials.setAdapter(adapter_tutorials);
+        lv_tutorials.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //这次设置点击跳转
+                ITutorial tutorial = (ITutorial) parent.getItemAtPosition(position);
+                String tutorial_id = tutorial.get_id();
+
+                Intent intent = new Intent(KnowledgeNetActivity.this, TutorialActivity.class);
+                intent.putExtra("tutorial_id", tutorial_id);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void bind_points() {
+        adapter_points = new KnowledgeNetPointListAdapter(this);
+        adapter_points.add_items(points);
+        lv_points.setAdapter(adapter_points);
+        lv_points.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //这次设置点击跳转
+                IKnowledgePoint knowledgePoint = (IKnowledgePoint) parent.getItemAtPosition(position);
+                String knowledge_point_id = knowledgePoint.get_id();
+
+                Intent intent = new Intent(KnowledgeNetActivity.this, KnowledgePointActivity.class);
+                intent.putExtra("knowledge_point_id", knowledge_point_id);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_tutorials) {
+            lv_tutorials.setVisibility(View.VISIBLE);
+            lv_points.setVisibility(View.GONE);
+            btn_tutorials.setChecked(true);
+            btn_points.setChecked(false);
+        } else {
+            lv_tutorials.setVisibility(View.GONE);
+            lv_points.setVisibility(View.VISIBLE);
+            btn_tutorials.setChecked(false);
+            btn_points.setChecked(true);
+        }
     }
 }
