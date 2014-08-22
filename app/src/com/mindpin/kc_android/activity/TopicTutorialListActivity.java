520f1dd434585a33404e1c98bae455e510a630bc
fushang318 <fushang318@gmail.com>
2014-08-22 17:51:12 +0800

    TopicTutorialListActivity ui

--- app/src/com/mindpin/kc_android/activity/TopicTutorialListActivity.java
+++ app/src/com/mindpin/kc_android/activity/TopicTutorialListActivity.java
@@ -1,7 +1,7 @@
 package com.mindpin.kc_android.activity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -11,7 +11,6 @@ import com.mindpin.kc_android.R;
 import com.mindpin.kc_android.activity.base.KnowledgeBaseActivity;
 import com.mindpin.kc_android.adapter.TopicTutorialListAdapter;
 import com.mindpin.kc_android.models.interfaces.ITopic;
-import com.mindpin.kc_android.network.HttpApi;
 import com.mindpin.kc_android.utils.KCAsyncTask;
 import com.mindpin.kc_android.utils.UiFont;
 import com.nostra13.universalimageloader.core.ImageLoader;
@@ -29,21 +28,27 @@ public class TopicTutorialListActivity extends KnowledgeBaseActivity{
     private ImageView topic_icon_img;
     private TextView topic_title_tv;
     private TextView topic_desc_tv;
+    private Bitmap topic_background;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.topic_tutorial_list);
 
-//        topic = (ITopic)getIntent().getSerializableExtra("topic");
+        topic = (ITopic)getIntent().getSerializableExtra("topic");
         loading_view = (LoadingView) findViewById(R.id.loading_view);
         topic_tutorial_list_view = (HListView) findViewById(R.id.topic_tutorial_list);
         topic_icon_img = (ImageView)findViewById(R.id.topic_icon_img);
         Button back_btn = (Button)findViewById(R.id.back_btn);
         back_btn.setTypeface(UiFont.FONTAWESOME_FONT);
 
+
         topic_title_tv = (TextView)findViewById(R.id.topic_title_tv);
         topic_desc_tv = (TextView)findViewById(R.id.topic_desc_tv);
+
+        topic_title_tv.setText(topic.get_title());
+        topic_desc_tv.setText(topic.get_desc());
+
         get_datas();
     }
 
@@ -57,25 +62,23 @@ public class TopicTutorialListActivity extends KnowledgeBaseActivity{
 
             @Override
             public Void call() throws Exception {
-                topic = HttpApi.get_topic_list().get(2);
                 topic.get_tutorial_list();
+                topic_background = ImageLoader.getInstance().loadImageSync(topic.get_icon_url());
                 return null;
             }
 
             @Override
             protected void onSuccess(Void aVoid) throws Exception {
                 build_view();
+                topic_icon_img.setImageBitmap(topic_background);
                 loading_view.hide();
             }
         }.execute();
     }
 
     private void build_view() {
-        ImageLoader.getInstance().displayImage(topic.get_icon_url(), topic_icon_img);
         TopicTutorialListAdapter adapter = new TopicTutorialListAdapter(this);
         adapter.add_items(topic.get_tutorial_list());
         topic_tutorial_list_view.setAdapter(adapter);
-        topic_title_tv.setText(topic.get_title());
-        topic_desc_tv.setText(topic.get_desc());
     }
 }
--- app/src/com/mindpin/kc_android/adapter/TopicTutorialListAdapter.java
+++ app/src/com/mindpin/kc_android/adapter/TopicTutorialListAdapter.java
@@ -1,12 +1,17 @@
 package com.mindpin.kc_android.adapter;
 
 import android.app.Activity;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.TextView;
 
 import com.mindpin.kc_android.R;
 import com.mindpin.kc_android.adapter.base.KnowledgeBaseAdapter;
 import com.mindpin.kc_android.models.interfaces.ITutorial;
+
+import it.sephiroth.android.library.widget.AbsHListView;
 
 /**
  * Created by fushang318 on 2014/8/22.
@@ -26,6 +31,15 @@ public class TopicTutorialListAdapter extends KnowledgeBaseAdapter<ITutorial> {
     public BaseViewHolder build_view_holder(View view) {
         ViewHolder view_holder = new ViewHolder();
         view_holder.tutorial_title_tv = (TextView) view.findViewById(R.id.tutorial_title_tv);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        activity.getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels * 45 / 100;
+        int height = width * 2;
+        AbsHListView.LayoutParams lp = new AbsHListView.LayoutParams(width, height);
+        view.setLayoutParams(lp);
+
         return view_holder;
     }
 
