4eff5774221a25dafdf163f5045d49ef8e07a16e
mykingla <kingla_pei@163.com>
2014-08-28 11:47:11 +0800

    fix cache config

--- app/src/com/mindpin/kc_android/application/KCApplication.java
+++ app/src/com/mindpin/kc_android/application/KCApplication.java
@@ -30,46 +30,43 @@ public class KCApplication extends Application {
 //                new ImageLoaderConfiguration.Builder(getApplicationContext()).build();
 
         DisplayImageOptions options;
+        ImageLoaderConfiguration config;
 
         File cache_dir = BaseUtils.create_dir(new File("/kc/cache/image"));
 
-        if (cache_dir == null) {
-            options = new DisplayImageOptions.Builder()
-                    .cacheInMemory(false)
-                    .cacheOnDisk(false)
-                    .build();
-        } else {
+        if (cache_dir != null) {
             options = new DisplayImageOptions.Builder()
                     .cacheInMemory(false)
                     .cacheOnDisk(true)
                     .build();
+
+            config = new ImageLoaderConfiguration.Builder(getApplicationContext())
+
+                    // 设置缓存图片的宽度跟高度
+                    .memoryCacheExtraOptions(480, 800)
+                    .diskCacheExtraOptions(480, 800, null)
+
+                    // 通过 LruMemoryCache 实现缓存机制
+                    .memoryCache(new LruMemoryCache(2 * 1024 * 1024))
+                    .memoryCacheSize(2 * 1024 * 1024)
+
+                            // 限制缓存文件数量百分比
+                    .memoryCacheSizePercentage(13)
+
+                    .diskCache(new UnlimitedDiscCache(cache_dir))
+                    .diskCacheSize(50 * 1024 * 1024)
+
+                    // 硬盘缓存文件数量
+                    .diskCacheFileCount(100)
+                    .diskCacheFileNameGenerator(new HashCodeFileNameGenerator())
+                    .defaultDisplayImageOptions(options)
+
+
+                    .build();
+
+            ImageLoader.getInstance().init(config);
         }
-        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext())
 
-            // 设置缓存图片的宽度跟高度
-            .memoryCacheExtraOptions(480, 800)
-            .diskCacheExtraOptions(480, 800, null)
-
-            // 通过 LruMemoryCache 实现缓存机制
-            .memoryCache(new LruMemoryCache(2 * 1024 * 1024))
-            .memoryCacheSize(2 * 1024 * 1024)
-
-            // 限制缓存文件数量百分比
-            .memoryCacheSizePercentage(13)
-            
-            .diskCache(new UnlimitedDiscCache(cache_dir)) // default
-            .diskCacheSize(50 * 1024 * 1024)
-
-            // 硬盘缓存文件数量
-            .diskCacheFileCount(100)
-            .diskCacheFileNameGenerator(new HashCodeFileNameGenerator())
-            .defaultDisplayImageOptions(options)
-
-
-            .build();
-
-
-        ImageLoader.getInstance().init(config);
 
         context = this.getApplicationContext();
 
