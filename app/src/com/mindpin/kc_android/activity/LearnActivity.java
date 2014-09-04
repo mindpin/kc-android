8068b088719d3e1cace4be8d1c69a4e38a306b5d
destinyd <destinyd_war@163.com>
2014-09-04 09:28:50 +0000

    select as requirement

--- app/src/com/mindpin/kc_android/activity/LearnActivity.java
+++ app/src/com/mindpin/kc_android/activity/LearnActivity.java
@@ -1,5 +1,6 @@
 package com.mindpin.kc_android.activity;
 
+import android.content.DialogInterface;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
@@ -41,11 +42,12 @@ public class LearnActivity extends KnowledgeBaseActivity {
 
     private IStep step_now;
     private String id_next_step;
-    private List<Button> list_buttons = new ArrayList<Button>();
+    private List<RadioButton> list_radios = new ArrayList<RadioButton>();
     private Button btn_next_step = null;
     private static int dp20 = BaseUtils.dp_to_px(10);
     private static int dp5 = BaseUtils.dp_to_px(2.5f);
     private LinearLayout.LayoutParams layoutParams, layoutParamsHeightWrap20dp, layoutParamsHeightWrap5dp;
+    RadioGroup radioGroup;
 
 
     @Override
@@ -106,39 +108,36 @@ public class LearnActivity extends KnowledgeBaseActivity {
                 tv_question.setTypeface(tv_question.getTypeface(), Typeface.BOLD);
                 ll_step.addView(tv_question, layoutParamsHeightWrap20dp);
 
-                RadioGroup radioGroup = new RadioGroup(this);
+                radioGroup = new RadioGroup(this);
                 List<IStep.ISelectOption> select_options = select.get_select_options();
                 for (IStep.ISelectOption option : select_options) {
-                    System.out.println(option.get_next_step_id());
-                    System.out.println(option.get_text());
                     RadioButton radio_button = new RadioButton(this);
                     radio_button.setText(option.get_text());
                     radio_button.setTag(option.get_next_step_id());
-                    radio_button.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            get_next_step((String) v.getTag());
-                            set_buttons_enable(false);
-                        }
-                    });
                     radio_button.setBackgroundColor(0xff999999);
                     radioGroup.addView(radio_button, layoutParams);
                     ((LinearLayout.LayoutParams)radio_button.getLayoutParams()).setMargins(0, dp5, 0, 0);
-//                    list_buttons.add(radio_button);
+                    list_radios.add(radio_button);
                 }
                 ll_step.addView(radioGroup, layoutParamsHeightWrap20dp);
 
+                btn_next_step = new Button(this);
+                btn_next_step.setText("确定");
+                btn_next_step.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                    if(radioGroup.getCheckedRadioButtonId() > -1) {
+                        btn_next_step.setEnabled(false);
+                        set_radios_enable(false);
+                        get_next_step(findViewById(radioGroup.getCheckedRadioButtonId()).getTag().toString());
+                    }
+                    else{
+                        Toast.makeText(LearnActivity.this, "你还未做选择", Toast.LENGTH_LONG).show();
+                    }
+                    }
+                });
 
-//                btn_next_step = new Button(this);
-//                btn_next_step.setText("确定");
-//                btn_next_step.setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View v) {
-//                        btn_next_step.setEnabled(false);
-//                        get_next_step(step.get_next_id());
-//                    }
-//                });
-
+                ll_step.addView(btn_next_step, layoutParamsHeightWrap20dp);
             }
             else if (step.get_continue_type() == IStep.ContinueType.STEP){
                 System.out.println("STEP");
@@ -153,15 +152,6 @@ public class LearnActivity extends KnowledgeBaseActivity {
                 });
                 ll_step.addView(btn_next_step, layoutParamsHeightWrap20dp);
             }
-            else if (step.get_continue_type() == IStep.ContinueType.END){
-                System.out.println("END");
-//                TextView textView = new TextView(this);
-//                textView.setText("教程结束啦");
-//                textView.setBackgroundColor(0xff229999);
-//                textView.setPadding(dp5, dp5, dp5, dp5);
-//                ll_step.addView(textView, layoutParams);
-//                ((LinearLayout.LayoutParams)textView.getLayoutParams()).setMargins(0, dp20, 0, 0);
-            }
 
             LinearLayout dividing = (LinearLayout)getLayoutInflater().inflate(R.layout.include_dotted_dividing_line, null);
             ll_step.addView(dividing, layoutParamsHeightWrap5dp);
@@ -169,6 +159,7 @@ public class LearnActivity extends KnowledgeBaseActivity {
             ll_steps.addView(ll_step, layoutParamsHeightWrap20dp);
 
             if (step.get_continue_type() == IStep.ContinueType.END) {
+                System.out.println("END");
                 TextView textView = new TextView(this);
                 textView.setText("教程结束啦");
                 textView.setBackgroundColor(0xff229999);
@@ -180,8 +171,6 @@ public class LearnActivity extends KnowledgeBaseActivity {
     }
 
     private TextView get_tv_block(String content) {
-        System.out.println("get_tv_block");
-        System.out.println(content);
         TextView textView = new TextView(this);
         textView.setText(content);
         return textView;
@@ -198,8 +187,6 @@ public class LearnActivity extends KnowledgeBaseActivity {
 
     private void get_next_step(String id) {
         id_next_step = id;
-        System.out.println("get_next_step");
-        System.out.println(id_next_step);
         new KCAsyncTask<IStep>(this) {
             @Override
             public IStep call() throws Exception {
@@ -209,21 +196,25 @@ public class LearnActivity extends KnowledgeBaseActivity {
 
             @Override
             protected void onSuccess(IStep step) throws Exception {
-                System.out.println(step.get_title());
                 if(btn_next_step != null && btn_next_step.getParent() != null)
                     ((ViewGroup)btn_next_step.getParent()).removeView(btn_next_step);
+                if(radioGroup != null) {
+                    int radioSelectId = radioGroup.getCheckedRadioButtonId();
+                    for (int i = radioGroup.getChildCount() - 1; i >= 0; i--) {
+                        View view = radioGroup.getChildAt(i);
+                        if (view.getId() != radioSelectId)
+                            radioGroup.removeViewAt(i);
+                    }
+                }
                 add_step(step);
                 step_now = step;
-                for (Button button : list_buttons) {
-                    button.setVisibility(View.GONE);
-                }
-                list_buttons.clear();
+                list_radios.clear();
             }
 
             @Override
             protected void onException(Exception e) throws RuntimeException {
                 super.onException(e);
-                set_buttons_enable(true);
+                set_radios_enable(true);
             }
         }.execute();
     }
@@ -256,9 +247,9 @@ public class LearnActivity extends KnowledgeBaseActivity {
         }.execute();
     }
 
-    private void set_buttons_enable(boolean b) {
-        for(Button button : list_buttons){
-            button.setEnabled(b);
+    private void set_radios_enable(boolean b) {
+        for(RadioButton radio : list_radios){
+            radio.setEnabled(b);
         }
     }
 }
