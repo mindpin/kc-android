a9a9feba9b403860ddcc266fe9a37c46710603db
destinyd <destinyd_war@163.com>
2014-10-09 04:01:35 +0000

    confirm when click note and question delete button

--- app/src/com/mindpin/kc_android/activity/NotesActivity.java
+++ app/src/com/mindpin/kc_android/activity/NotesActivity.java
@@ -1,74 +1,22 @@
 package com.mindpin.kc_android.activity;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.os.Bundle;
 import android.view.View;
-import android.widget.LinearLayout;
 import com.mindpin.kc_android.R;
 import com.mindpin.kc_android.activity.base.KnowledgeBaseWriteActivity;
 import com.mindpin.kc_android.models.interfaces.INote;
-import com.mindpin.kc_android.utils.KCAsyncTask;
 
 /**
  * Created by dd on 14-9-25.
  */
 public class NotesActivity extends KnowledgeBaseWriteActivity<INote> implements View.OnClickListener {
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    protected void get_data() {
-        new KCAsyncTask<Void>(this) {
-
-            @Override
-            protected void onPreExecute() throws Exception {
-                loading_view.show();
-            }
-
-            @Override
-            public Void call() throws Exception {
-                clazz = step.get_note();
-                return null;
-            }
-
-            @Override
-            protected void onSuccess(Void aVoid) throws Exception {
-                build_view();
-                loading_view.hide();
-            }
-        }.execute();
-    }
-
-    @Override
-    protected String get_default_write_text() {
-        try {
-            return clazz == null ?  "" : clazz.get_content();
-        } catch (Exception ex) {
-            return "";
-        }
+    protected INote get_data() {
+        return step.get_note();
     }
 
     @Override
     protected int get_edit_text_write_hint_res_id() {
         return R.string.notes_edit_text_hint;
-    }
-
-    @Override
-    protected View.OnClickListener get_button_cancel_on_click_listener(View btn_cancel) {
-        return this;
-    }
-
-    @Override
-    protected View.OnClickListener get_button_save_on_click_listener(View btn_save) {
-        return this;
-    }
-
-    @Override
-    protected View.OnClickListener get_actionbar_button_on_click_listener(View btn_write_actionbar) {
-        return this;
     }
 
     @Override
@@ -95,87 +43,16 @@ public class NotesActivity extends KnowledgeBaseWriteActivity<INote> implements 
     }
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn_cancel:
-                System.out.println("btn_cancel");
-                finish();
-                break;
-            case R.id.btn_save:
-                System.out.println("btn_save");
-                if("".equals(et_write.getText().toString())){
-                    new AlertDialog.Builder(this).setTitle("提示")
-                            .setMessage("未输入任何内容")
-                            .setNegativeButton("取消提交", null)
-                            .setPositiveButton("退出页面", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    finish();
-                                }
-                            }).create().show();
-                    return;
-                }
-                save(v);
-                break;
-            case R.id.btn_delete:
-                delete(v);
-                break;
+    protected void destroy() {
+        step.destroy_note();
+    }
+
+    @Override
+    protected void save() {
+        if (step.has_note()) {
+            step.edit_note(et_write.getText().toString());
+        } else {
+            step.create_note(et_write.getText().toString());
         }
-    }
-
-    private void delete(final View v) {
-        new KCAsyncTask<Void>(this) {
-            @Override
-            protected void onPreExecute() throws Exception {
-                v.setEnabled(false);
-            }
-
-            @Override
-            public Void call() throws Exception {
-                step.destroy_note();
-                return null;
-            }
-
-            @Override
-            protected void onSuccess(Void aVoid) throws Exception {
-                finish_with_result();
-            }
-
-            @Override
-            protected void onException(Exception e) throws RuntimeException {
-                super.onException(e);
-                v.setEnabled(true);
-            }
-        }.execute();
-    }
-
-    private void save(final View v) {
-        new KCAsyncTask<Void>(this) {
-            @Override
-            protected void onPreExecute() throws Exception {
-                v.setEnabled(false);
-            }
-
-            @Override
-            public Void call() throws Exception {
-                if (step.has_note()) {
-                    step.edit_note(et_write.getText().toString());
-                } else {
-                    step.create_note(et_write.getText().toString());
-                }
-                return null;
-            }
-
-            @Override
-            protected void onSuccess(Void aVoid) throws Exception {
-                finish_with_result();
-            }
-
-            @Override
-            protected void onException(Exception e) throws RuntimeException {
-                super.onException(e);
-                v.setEnabled(true);
-            }
-        }.execute();
     }
 }
--- app/src/com/mindpin/kc_android/activity/base/KnowledgeBaseWriteActivity.java
+++ app/src/com/mindpin/kc_android/activity/base/KnowledgeBaseWriteActivity.java
@@ -1,5 +1,7 @@
 package com.mindpin.kc_android.activity.base;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +11,7 @@ import android.widget.TextView;
 import com.mindpin.android.loadingview.LoadingView;
 import com.mindpin.kc_android.R;
 import com.mindpin.kc_android.models.http.Step;
+import com.mindpin.kc_android.utils.KCAsyncTask;
 import com.mindpin.kc_android.widget.FontAwesomeTextView;
 import roboguice.inject.InjectExtra;
 import roboguice.inject.InjectView;
@@ -16,43 +19,69 @@ import roboguice.inject.InjectView;
 /**
  * Created by fushang318 on 2014/8/14.
  */
-public abstract class KnowledgeBaseWriteActivity<Clazz> extends KnowledgeBaseActivity{
+public abstract class KnowledgeBaseWriteActivity<Clazz> extends KnowledgeBaseActivity implements View.OnClickListener {
     private static final int CODE_STEP_ACTIONS = 0;
     @InjectExtra("step")
     protected Step step;
     @InjectView(R.id.et_write)
     protected EditText et_write;
+    @InjectView(R.id.btn_delete)
+    protected Button btn_delete;
+    @InjectView(R.id.btn_save)
+    protected Button btn_save;
+    @InjectView(R.id.btn_cancel)
+    protected Button btn_cancel;
+    @InjectView(R.id.loading_view)
+    protected LoadingView loading_view;
+    protected Intent mIntent;
+    protected Clazz clazz = null;
     @InjectView(R.id.fatv_write_actionbar_icon)
     FontAwesomeTextView fatv_write_actionbar_icon;
     @InjectView(R.id.tv_write_actionbar_title)
     TextView tv_write_actionbar_title;
-    @InjectView(R.id.btn_delete)
-    Button btn_write_actionbar;
-    @InjectView(R.id.btn_save)
-    Button btn_save;
-    @InjectView(R.id.btn_cancel)
-    Button btn_cancel;
-
-    @InjectView(R.id.loading_view)
-    protected LoadingView loading_view;
-
-    protected Intent mIntent;
-    protected Clazz clazz = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.write);
-        get_data();
+        bind_views();
+        get_data_base();
         mIntent = new Intent();
         mIntent.putExtra("step", step);
     }
 
-    protected abstract void get_data();
+    private void get_data_base() {
+        new KCAsyncTask<Void>(this) {
+
+            @Override
+            protected void onPreExecute() throws Exception {
+                loading_view.show();
+            }
+
+            @Override
+            public Void call() throws Exception {
+                clazz = get_data();
+                return null;
+            }
+
+            @Override
+            protected void onSuccess(Void aVoid) throws Exception {
+                build_view();
+                loading_view.hide();
+            }
+        }.execute();
+    }
+
+    private void bind_views() {
+        btn_cancel.setOnClickListener(this);
+        btn_delete.setOnClickListener(this);
+        btn_save.setOnClickListener(this);
+    }
+
+    protected abstract Clazz get_data();
 
     protected void build_view() {
         build_actionbar();
-        build_bottom();
         build_edit_text_write();
     }
 
@@ -61,18 +90,12 @@ public abstract class KnowledgeBaseWriteActivity<Clazz> extends KnowledgeBaseAct
         et_write.setText(get_default_write_text());
     }
 
-    private void build_bottom() {
-        btn_save.setOnClickListener(get_button_save_on_click_listener(btn_save));
-        btn_cancel.setOnClickListener(get_button_cancel_on_click_listener(btn_cancel));
-    }
-
     private void build_actionbar() {
         fatv_write_actionbar_icon.setText(get_title_icon_res_id());
         tv_write_actionbar_title.setText(get_title_res_id());
-        btn_write_actionbar.setText(get_actionbar_button_text_res_id());
-        btn_write_actionbar.setOnClickListener(get_actionbar_button_on_click_listener(btn_write_actionbar));
-        if(!is_actionbar_button_show())
-            btn_write_actionbar.setVisibility(View.INVISIBLE);
+        btn_delete.setText(get_actionbar_button_text_res_id());
+        if (!is_actionbar_button_show())
+            btn_delete.setVisibility(View.INVISIBLE);
 
     }
 
@@ -88,21 +111,120 @@ public abstract class KnowledgeBaseWriteActivity<Clazz> extends KnowledgeBaseAct
         super.finish();
     }
 
-    protected abstract String get_default_write_text();
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_cancel:
+                System.out.println("btn_cancel");
+                finish();
+                return;
+            case R.id.btn_save:
+                if ("".equals(et_write.getText().toString())) {
+                    new AlertDialog.Builder(this).setTitle("提示")
+                            .setMessage("未输入任何内容")
+                            .setNegativeButton("取消提交", null)
+                            .setPositiveButton("退出页面", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    finish();
+                                }
+                            }).create().show();
+                    return;
+                }
+                save_base();
+                return;
+            case R.id.btn_delete:
+                confirm_delete();
+                break;
+        }
+    }
+
+    private void save_base() {
+        new KCAsyncTask<Void>(this) {
+            @Override
+            protected void onPreExecute() throws Exception {
+                btn_save.setEnabled(false);
+            }
+
+            @Override
+            public Void call() throws Exception {
+                save();
+                return null;
+            }
+
+            @Override
+            protected void onSuccess(Void aVoid) throws Exception {
+                finish_with_result();
+            }
+
+            @Override
+            protected void onException(Exception e) throws RuntimeException {
+                super.onException(e);
+                btn_save.setEnabled(true);
+            }
+        }.execute();
+    }
+
+    private void confirm_delete() {
+        new AlertDialog.Builder(this).setTitle("提示")
+                .setMessage("您确定要删除？")
+                .setNegativeButton("取消", null)
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        destroy_base();
+                    }
+                }).create().show();
+        return;
+    }
+
+    private void destroy_base() {
+        new KCAsyncTask<Void>(this) {
+            @Override
+            protected void onPreExecute() throws Exception {
+                btn_delete.setEnabled(false);
+            }
+
+            @Override
+            public Void call() throws Exception {
+                destroy();
+                return null;
+            }
+
+            @Override
+            protected void onSuccess(Void aVoid) throws Exception {
+                finish_with_result();
+            }
+
+            @Override
+            protected void onException(Exception e) throws RuntimeException {
+                super.onException(e);
+                btn_delete.setEnabled(true);
+            }
+        }.execute();
+    }
+
+    protected abstract void save();
+
+    protected String get_default_write_text() {
+        try {
+            return clazz == null ? "" : (String) clazz.getClass().getMethod("get_content", new Class[]{}).invoke(clazz, new Object[]{});
+        } catch (Exception ex) {
+            return "";
+        }
+    }
+
+    ;
 
     protected abstract boolean is_actionbar_button_show();
 
     protected abstract int get_edit_text_write_hint_res_id();
-
-    protected abstract View.OnClickListener get_button_cancel_on_click_listener(View btn_cancel);
-
-    protected abstract View.OnClickListener get_button_save_on_click_listener(View btn_save);
-
-    protected abstract View.OnClickListener get_actionbar_button_on_click_listener(View btn_write_actionbar);
 
     protected abstract int get_actionbar_button_text_res_id();
 
     protected abstract int get_title_res_id();
 
     protected abstract int get_title_icon_res_id();
+
+    protected abstract void destroy();
 }
